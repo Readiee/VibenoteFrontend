6a88daceb48f1d5c0c828f59177683ce49db7a3a
Readiee <galsanov.bul888@gmail.com>
2025-04-24 07:40:51 +0300

    App renaming (VibeNote -> Vibenote)

--- app/src/main/java/com/vbteam/vibenote/MainActivity.kt
+++ app/src/main/java/com/vbteam/vibenote/MainActivity.kt
@@ -3,25 +3,13 @@ package com.vbteam.vibenote
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.View
-import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.compose.rememberNavController
 import com.vbteam.vibenote.navigation.NavGraph
-import com.vbteam.vibenote.ui.theme.VibeNoteTheme
+import com.vbteam.vibenote.ui.theme.VibenoteTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -45,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContent {
-            VibeNoteTheme {
+            VibenoteTheme {
                 val navController = rememberNavController()
                 NavGraph(navController)
             }
